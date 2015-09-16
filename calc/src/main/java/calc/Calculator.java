6a485756d789b9a7c415c70091db46dcb4500550
maksim.bobylev <maksim.bobylev@W1-0091.walletone.local>
2015-09-16 14:55:56 +0600

    from netbeans

--- calc/src/main/java/calc/Calculator.java
+++ calc/src/main/java/calc/Calculator.java
@@ -86,7 +86,8 @@ public class Calculator {
 	
 	public String getNumber1(){
 		String num1 = String.valueOf(number1);
-		if (!num1.contains("E")) {
+		System.out.println("just test");
+                if (!num1.contains("E")) {
 			return num1.replaceAll("\\.?0*$", ""); // удаляем лишние нули после точки
 		} else return num1;
 	}
